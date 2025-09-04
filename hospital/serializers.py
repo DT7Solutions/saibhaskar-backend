b95896538740dc252b89c992453a2d59ef069895
website-krysta <website.krysta@gmail.com>
2025-09-04 10:43:54 +0530

    fixed all chnages

--- hospital/serializers.py
+++ hospital/serializers.py
@@ -5,3 +5,8 @@ class UsersSerializer(serializers.ModelSerializer):
     class Meta:
         model = Users
         fields = '__all__'
+
+
+class UserLoginSerializer(serializers.Serializer):
+    email = serializers.EmailField()
+    password = serializers.CharField(write_only=True)
--- hospital/views.py
+++ hospital/views.py
@@ -1,10 +1,15 @@
 from django.shortcuts import render
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
-from rest_framework import status
+from rest_framework import status,generics
 from .models import *
+from django.contrib.auth import authenticate,get_user_model
+from .serializers import *
+from rest_framework_simplejwt.tokens import RefreshToken
+
 # Create your views here.
-from .serializers import UsersSerializer
+
+
 
 # ✅ Get all users
 @api_view(['GET'])
@@ -23,3 +28,21 @@ def get_user_detail(request, pk):
     
     serializer = UsersSerializer(user)
     return Response(serializer.data, status=status.HTTP_200_OK)
+
+class LoginView(generics.GenericAPIView):
+    serializer_class = UserLoginSerializer
+
+    def post(self, request, *args, **kwargs):
+        email = request.data.get('email')
+        password = request.data.get('password')
+
+        user = authenticate(email=email, password=password)
+        if user:
+            refresh = RefreshToken.for_user(user)
+            return Response({
+                'refresh': str(refresh),
+                'access': str(refresh.access_token),
+                'message': 'Login successful',
+                'role': user.role.role_category if user.role else None, 
+            }, status=status.HTTP_200_OK)
+        return Response({'error': 'Invalid Credentials'}, status=status.HTTP_401_UNAUTHORIZED)
