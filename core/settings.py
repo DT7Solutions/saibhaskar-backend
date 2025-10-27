1971b5537498419449b9af8eee4de90b23de7711
Hemanthallam <hemanthallam009@gmail.com>
2025-10-27 15:51:23 +0530

    removing slash

--- core/settings.py
+++ core/settings.py
@@ -35,7 +35,7 @@ ALLOWED_HOSTS = ['91.108.110.206','api.saibhaskarhospital.com','www.api.saibhask
 CORS_ALLOW_CREDENTIALS = True
 CORS_ALLOW_ALL_ORIGINS = True
 CORS_ALLOWED_ORIGINS = [
-    "https://saibhaskarhospital.com/",  
+    "https://saibhaskarhospital.com",  
 ]
 # https://frontend.codkraft.com 
 # https://127.0.0.1:5500 
