bd7a3c842b9fb44b9cac10a07363110bdee2d7ca
Hemanthallam <hemanthallam009@gmail.com>
2025-10-27 14:54:17 +0530

    changed database pl/sql

--- core/settings.py
+++ core/settings.py
@@ -29,12 +29,13 @@ DEBUG = False
 # False
 ALLOWED_HOSTS = ['91.108.110.206','api.saibhaskarhospital.com','www.api.saibhaskarhospital.com']
 # '91.108.110.206','codkraft.com','www.codkraft.com'
+# '91.108.110.206','api.saibhaskarhospital.com','www.api.saibhaskarhospital.com'
 
 
 CORS_ALLOW_CREDENTIALS = True
 CORS_ALLOW_ALL_ORIGINS = True
 CORS_ALLOWED_ORIGINS = [
-    "https://saibhaskarhospital.com",  
+    "https://saibhaskarhospital.com/",  
 ]
 # https://frontend.codkraft.com 
 # https://127.0.0.1:5500 
@@ -102,11 +103,22 @@ WSGI_APPLICATION = 'core.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+'default': {
+'ENGINE': 'django.db.backends.postgresql',
+'NAME': 'bmrdb',
+'USER': 'bmruser',
+'PASSWORD': 'BmrHospital@2025',
+'HOST': '193.203.160.126',
+'PORT': '5432',
+}
 }
 
 
